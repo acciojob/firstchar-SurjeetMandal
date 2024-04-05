fa3b8e88eae2f23da0c5dd587e5f2ac895ff9820
SurjeetMandal <surjeetmandal698@gmail.com>
2024-04-05 10:34:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function firstChar(text) {
-  // your code here
+	let res = text.trim();
+	let finRes = res.charAt(0);
+	return finRes;
 }
 
 // Do not change the code below
 
 const text = prompt("Enter text:");
 alert(firstChar(text));
+
